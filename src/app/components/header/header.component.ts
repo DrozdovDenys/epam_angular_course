3d4a7e2be948049bd39c587623f84c48d05414b0
DrozdovDenys <denysjsjr@gmail.com>
2023-05-22 15:35:17 +0300

    added mock data for user

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { user } from 'src/app/data/user';
+import { IUser } from 'src/app/models/user';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  title = 'Video Course';
-  userName = 'User';
+	title = 'Video Course';
+	userName: IUser = user;
 }
